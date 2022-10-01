77798afa26f8f796cd08e42c219da91123bc27db
neokii <neokii@gmail.com>
2022-10-01 16:08:53 +0900

    remove unused parts

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -79,16 +79,11 @@ protected:
   void drawBottomIcons(QPainter &p);
   void drawSteer(QPainter &p);
   void drawThermal(QPainter &p);
-  void drawRestArea(QPainter &p);
   void drawTurnSignals(QPainter &p);
   void drawGpsStatus(QPainter &p);
   void drawDebugText(QPainter &p);
   void drawHud(QPainter &p);
 
-private:
-  QPixmap get_icon_iol_com(const char* key);
-  void drawRestAreaItem(QPainter &p, int yPos, capnp::Text::Reader image, capnp::Text::Reader title,
-                        capnp::Text::Reader oilPrice, capnp::Text::Reader distance, bool lastItem);
 };
 
 // container for all onroad widgets
